5bb634113fa83152c12b82912853eb08da7dd453
YuYoung <yuyoung32@qq.com>
2025-03-16 16:32:43 +0800

    [Feature] draw 2 2D textures

--- src/main.cpp
+++ src/main.cpp
@@ -52,11 +52,11 @@ int main()
 
     // VBO
     float vertices[] = {
-            // positions          // colors           // texture coords
-            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-            0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+            // positions          // colors           // texture0 coords
+            0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+            0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+            -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
     };
     // 不是必须使用VBO，这只是CPU->GPU的一个过程，画之前需要穿一次数据给GPU。这里预先传给GPU方便后续直接使用。
     unsigned int VBO; // 顶点缓冲对象：Vertex Buffer Object，VBO
@@ -79,24 +79,27 @@ int main()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); // 针对某种类型的槽位里面的东西做操作
 
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
     // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-    // texture coord attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    // texture0 coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 
     // Texture
     int width, height, nrChannels;
+
     unsigned char *data = stbi_load("../src/assets/container.jpg", &width, &height, &nrChannels, 0);
-    if (data != nullptr) {
-        LOGI("load png successfully, w={} h={} nrChannels={}", width, height, nrChannels);
+    if (data) {
+        LOGI("Load assets container");
     }
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    unsigned int texture0;
+    glGenTextures(1, &texture0);
+    LOGI("glGenTextures texture0={}", texture0);
+    glActiveTexture(GL_TEXTURE0); // 在绑定纹理之前先激活纹理单元
+    glBindTexture(GL_TEXTURE_2D, texture0);
     // 纹理超出后的环绕方式 S T R 类似 X Y Z
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
@@ -106,8 +109,30 @@ int main()
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
     stbi_image_free(data);
+    data = nullptr;
 
+    data = stbi_load("../src/assets/awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data) {
+        LOGI("Load assets awesomeface");
+    }
+    unsigned int texture1;
+    glGenTextures(1, &texture1);
+    LOGI("glGenTextures texture1={}", texture1);
+    glActiveTexture(GL_TEXTURE1); // 在绑定纹理之前先激活纹理单元
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // 纹理超出后的环绕方式 S T R 类似 X Y Z
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+    // 纹理缩放后的采样方式
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(data);
+
+    shader.setInt("texture1", 0); // 0代表GL_TEXTURE0
+    shader.setInt("texture2", 1); // 1代表GL_TEXTURE0
 
     while (!glfwWindowShouldClose(window)) {
         // 输入
@@ -120,9 +145,10 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f); // 设置状态：清除后的颜色是什么？
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBindTexture(GL_TEXTURE_2D, texture);
+//        glBindTexture(GL_TEXTURE_2D, texture0);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // glDrawElements 会使用glBindVertexArray和GL_ELEMENT_ARRAY_BUFFER 这两个槽位里的东西
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,
+                       0); // glDrawElements 会使用glBindVertexArray和GL_ELEMENT_ARRAY_BUFFER 这两个槽位里的东西
 
         // 检查并调用事件，交换缓冲
         glfwPollEvents();
